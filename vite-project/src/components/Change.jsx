fb794d6708bd522f387c1b473aa9ba6321fdb578
lourranny2904 <santana.lourranny@escolar.ifrn.edu.br>
2025-05-14 17:49:03 -0300

    novo

--- vite-project/src/components/Change.jsx
+++ vite-project/src/components/Change.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react' // tem que ser importado e tem que usar uma variavel pois ela manipula cada variavel 
 export default function Change(){
-    let [valor, setValor] = useState(0)
+
+
+    let [valor, setValor] = useState('nome')
+    let [valor2, setValor2] = useState(1)
+
+    function sets(){
+        return(
+            setValor(valor+valor2),
+            setValor2(valor2 + 1)
+            )
+
+    }
     return(
         <div>
-            <button onClick = {() => setValor(valor+1)}>{valor}Cliques</button>
+            <button onClick = {sets} >{valor}Cliques</button>
+            <p>{valor2}</p>
         </div>
     )
 }
