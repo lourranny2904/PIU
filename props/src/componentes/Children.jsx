52db428d720ac61fdec745c3c35a95fb3991fa6f
lourranny <lourranny>
2025-05-15 14:07:31 -0300

    novo projeto

---
+++ props/src/componentes/Children.jsx
@@ -0,0 +1,9 @@
+//recebimen
+export default function Children ({children}){
+
+    return(
+        <div style={{border: "4px solid red", padding:"20px"}}>
+            {children}
+        </div>
+    )
+}
