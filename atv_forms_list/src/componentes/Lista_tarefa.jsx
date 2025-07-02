ee2b970978b7a744dbe67e66500b5f87ab3be9eb
lourranny <santana@lourranny@escolar.ifrn.edu.br>
2025-07-02 09:55:15 -0300

    atv de forms-list tarefas

--- atv_forms_list/src/componentes/Lista_tarefa.jsx
+++ atv_forms_list/src/componentes/Lista_tarefa.jsx
@@ -26,6 +26,20 @@ export default function Lista_tarefa() {
     listaTarefa(novaLista);
   };
 
+  const moverTarefa = (index, direcao) => {
+    const novaLista = [...lista];
+    const novoIndex = index + direcao;
+  
+    if (novoIndex < 0 || novoIndex >= lista.length) return;
+  
+    const temp = novaLista[index];
+    novaLista[index] = novaLista[novoIndex];
+    novaLista[novoIndex] = temp;
+  
+    listaTarefa(novaLista);
+  };
+  
+
   return (
     <div className='container'>
       <form onSubmit={handleSubmit}>
@@ -42,9 +56,16 @@ export default function Lista_tarefa() {
           <div key={index} className={`tarefas ${item.estado}`}>
             <li>
               {item.texto}
-              <button onClick={() => mudarEstado(index, 'realizada')}>realizada</button>
-              <button onClick={() => mudarEstado(index, 'naoRealizada')}>não realizada</button>
-              <button onClick={() => mudarEstado(index, 'pendente')}>pendente</button>
+
+              <div className='botoes-estado'>
+                <button onClick={() => mudarEstado(index, 'realizada')}>realizada</button>
+                <button onClick={() => mudarEstado(index, 'naoRealizada')}>não realizada</button>
+                <button onClick={() => mudarEstado(index, 'pendente')}>pendente</button>
+              </div>
+              <div className="botoes-mover">
+                <button onClick={() => moverTarefa(index, -1)}>↑</button>
+                <button onClick={() => moverTarefa(index, 1)}>↓</button>
+              </div>
             </li>
           </div>
         ))}
