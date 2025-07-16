3f81afe6df3ecdb7a129638f4ec789c214a312e1
lourranny <santana.lourranny@escolar.ifrn.edu.br>
2025-07-16 17:49:20 -0300

    novo assunto

---
+++ Use_Effect/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import CountClicks from './componentes/CountClicks'
+
+
+
+function App() {
+
+  return (
+    <>
+      <CountClicks/>
+      {/* <EffectAPI /> */}
+      {/* <RepoJson /> */}
+      
+    </>
+  )
+}
+
+export default App
